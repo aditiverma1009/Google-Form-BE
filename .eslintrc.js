72e63d227f7ef9dba480b4a3c3c32ae92bd23055
aditiverma1009 <aditiverma1009@gmail.com>
2018-03-10 20:38:33 +0530

    Backend Setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules":{
+      "no-console":0
+    },
+    "env":{
+      "jest":true
+    },
+    "rules":{
+      "jsx-a11y/href-no-hash": 0,
+    }
+};
