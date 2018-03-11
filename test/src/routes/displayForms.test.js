b26261dfeb035716138d8257296a8a079625a88a
aditiverma1009 <aditiverma1009@gmail.com>
2018-03-11 13:14:22 +0530

    edit tests to include model functions

--- test/src/routes/displayForms.test.js
+++ test/src/routes/displayForms.test.js
@@ -1,32 +1,33 @@
-// const Models = require('../../../models');
-// const server = require('../../../server');
+const Models = require('../../../models');
+const server = require('../../../server');
 
-// const option1 = {
-//   method: 'GET',
-//   url: '/forms/display',
-// };
+const option1 = {
+  method: 'GET',
+  url: '/forms/display',
+};
 
-// describe('Test for displayForm Response when there are forms in the table', () => {
-//   beforeAll(done => Models.forms.destroy({ truncate: true })
-//     .then(() => Models.forms.create({
-//       formtitle: 'Test Ques',
-//     }).then(() => done())));
+describe('Test for displayForm Response when there are no forms in the database', () => {
+  beforeAll(done => Models.forms.destroyAllObjects()
+    .then(() => done()));
 
-//   test('Responds with 200 code', testdone => server.inject(option1, (resp) => {
-//     expect(resp.result.code).toBe(200);
-//     testdone();
-//   }));
+  test('Responds with 200 code', testdone => server.inject(option1, (resp) => {
+    expect(resp.result.code).toBe(200);
+    testdone();
+  }));
+});
 
-//   afterAll(done => Models.forms.destroy({ truncate: true })
+describe('Test for displayForm Response when there are forms in the table', () => {
+  beforeAll(done => Models.forms.destroyAllObjects()
+    .then(() => Models.forms.createObject({
+      formtitle: 'Test Ques',
+    }).then(() => done())));
+
+  test('Responds with 200 code', testdone => server.inject(option1, (resp) => {
+    expect(resp.result.code).toBe(200);
+    testdone();
+  }));
+
+//   afterAll(done => Models.forms.destroyAllObjects()
 //     .then(() => done()));
-// });
+});
 
-// describe('Test for displayForm Response when there are no forms in the database', () => {
-//   beforeAll(done => Models.forms.destroy({ truncate: true })
-//     .then(() => done()));
-
-//   test('Responds with 200 code', testdone => server.inject(option1, (resp) => {
-//     expect(resp.result.code).toBe(200);
-//     testdone();
-//   }));
-// });
